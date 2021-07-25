b3168b50a206386dd39db4ff25cd4cf871df4bcf
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-26 01:08:13 +0200

    Update for switch to home page

--- portfolio-new/src/App.js
+++ portfolio-new/src/App.js
@@ -1,20 +1,19 @@
 import styled from 'styled-components'
 import Sidebar from './Components/Sidebar';
+import HomePage from './pages/HomePage';
+import { Route, Switch as Switching } from "react-router";
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
       <MainContentStyled>
-        <div className="lines">
-          <div className="line-1"></div>
-          <div className="line-2"></div>
-          <div className="line-3"></div>
-          <div className="line-4"></div>
-        </div>
-
+        <Switching>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+          </Switching>
       </MainContentStyled>
-
     </div>
   );
 }
@@ -22,19 +21,7 @@ const MainContentStyled = styled.main`
   position: relative;
   margin-left: 16.3rem;
   min-height: 100vh;
-
-  .lines{
-    position: absolute;
-    min-height: 100vh;
-    width: 100%;
-    display: flex;
-    justify-content: space-evenly;
-    .line-1, .line-2, .line-3, .line-4 {
-      width: 1px;
-      min-height: 100vh;
-      background-color: var(--border-color);
-    }
-  }
+  
 `
 
 export default App;
