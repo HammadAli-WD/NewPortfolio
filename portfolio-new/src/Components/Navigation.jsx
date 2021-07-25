7ec176e18d3e60aca73ca1d2a261cf837d6c6569
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-25 14:18:39 +0200

    Navigation links update

--- portfolio-new/src/Components/Navigation.jsx
+++ portfolio-new/src/Components/Navigation.jsx
@@ -1,51 +1,50 @@
 import React from 'react'
 import styled from 'styled-components'
 import avatar from '../images/avatar.jpg'
-import { NavLink } from "react-router-dom";
-export default function Navigation() {
+import { Link } from "react-router-dom";
+function Navigation() {
     return (
-        <NavigationStyles>
+        <NavigationStyled>
             <div className="avatar">
                 <img src={avatar} alt="" />
-                <ul className="nav-items">
-                    <li className="nav-items">
-                        <NavLink to="/" activeClassName="active-class" exact>Home</NavLink>
-                    </li>
-                    <li className="nav-items">
-                        <NavLink to="/" activeClassName="active-class" exact>About</NavLink>
-                    </li>
-                    <li className="nav-items">
-                        <NavLink to="/" activeClassName="active-class" exact>Experience</NavLink>
-                    </li>
-                    <li className="nav-items">
-                        <NavLink to="/" activeClassName="active-class" exact>Projects</NavLink>
-                    </li>
-                    <li className="nav-items">
-                        <NavLink to="/" activeClassName="active-class" exact>Blogs</NavLink>
-                    </li>
-                    <li className="nav-items">
-                        <NavLink to="/" activeClassName="active-class" exact>Contact</NavLink>
-                    </li>
-                </ul>
-                <footer className="footer">
-                    <a href="" target="_blank" rel="noreferrer"> Copyright &copy; {new Date().getFullYear()} {<br />} Hammad Ali </a>
-                </footer>
             </div>
-        </NavigationStyles>
+            <ul className="nav-items">
+                <li className="nav-items">
+                    <Link to="/home" activeClassName="active-class" exact>Home</Link>
+                </li>
+                <li className="nav-items">
+                    <Link to="/home" activeClassName="active-class" exact>About</Link>
+                </li>
+                <li className="nav-items">
+                    <Link to="/home" activeClassName="active-class" exact>Experience</Link>
+                </li>
+                <li className="nav-items">
+                    <Link to="/home" activeClassName="active-class" exact>Projects</Link>
+                </li>
+                <li className="nav-items">
+                    <Link to="/home" activeClassName="active-class" exact>Blogs</Link>
+                </li>
+                <li className="nav-items">
+                    <Link to="/home" activeClassName="active-class" exact>Contact</Link>
+                </li>
+            </ul>
+            <footer className="footer">
+                <a href="" target="_blank" rel="noreferrer"> Copyright &copy; {new Date().getFullYear()} {<br />} Hammad Ali </a>
+            </footer>
+
+        </NavigationStyled>
     )
 }
 
-const NavigationStyles = styled.nav`
-display: flex;
-justify-content: space-between;
-flex-direction: column;
-align-items: center;
-height: 100%;
-width: 100%;
-border-right: 1px solid var(--border-color);
-.avatar{
-    width: 100%;
-    border-bottom: 1px solid var(--border-color);
+const NavigationStyled = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    .avatar{
+        width: 100%;
+        border-bottom: 1px solid var(--border-color);
         text-align: center;
         padding: 1rem 0;
         img{
@@ -53,57 +52,58 @@ border-right: 1px solid var(--border-color);
             border-radius: 50%;
             border: 8px solid var(--border-color);
         }
-}
-.nav-items{
-            width: 100%;
-            .nav-item{
-                list-style: none;
-                text-align: center;
-
-                a{
-                    text-decoration:none;
-                    font-size: inherit;
-                    color: inherit;
-                    text-align: center;
-                    display: block;
-                    padding: .5rem 0;
-                    position: relative;
-                    &::before{
-                         content:'';
-                         position: absolute;
-                         top: 0;
-                         left: 0;
-                         width: .4rem;
-                         height: 100%;
-                         background-color: #0381ff33;
-                         transform-origin: bottom ;
-                         transform: scale(0);
-                         overflow-x:hidden ;
-                         transition: transform .2s, .2s width .06s cubic-bezier(1,-0.16,0,1.35);
-
-                    }
-
-                    &:hover::before{
-                        width: 100%;
-                        transform: scale(1);
-
-                    }
+    }
+    .nav-items{
+        width: 100%;
+        text-align: center;
+        .active-class{
+            background-color: var(--primary-color-light);
+            color: white;
+        }
+        li{
+            display: block;
+            a{
+                display: block;
+                padding: .45rem 0;
+                position: relative;
+                z-index: 10;
+                text-transform: uppercase;
+                transition: all .4s ease-in-out;
+                font-weight: 600;
+                letter-spacing: 1px;
+                &:hover{
+                    cursor: pointer;
+                    color: var(--white-color);
+                }
+                &::before{
+                    content: "";
+                    position: absolute;
+                    bottom: 0;
+                    left: 0;
+                    width: 0;
+                    height: 50%;
+                    background-color: var( --primary-color);
+                    transition: All 0.5s cubic-bezier(1,-0.3,.25,.92) ;
+                    opacity: 0.25;
+                    z-index: -1;
                 }
             }
-        }
-
-        .footer{
-            width:100%;
-            text-align:center;
-            border-top: 1px solid #2e344e;
-            a{
-                color:inherit;
-                width:55%;
-                font-size: 1.1rem;
-                text-align:center;
-                padding:1rem 0;
-
+            a:hover::before{
+                width: 100%;
+                height: 100%;
             }
-
         }
+    }
+    footer{
+        border-top: 1px solid var(--border-color);
+        width: 100%;
+        p{
+            padding: 1.3rem 0;
+            font-size: 1.1rem;
+            display: block;
+            text-align: center;
+        }
+    }
 `
+
+export default Navigation
