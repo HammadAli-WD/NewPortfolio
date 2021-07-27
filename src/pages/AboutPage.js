c56b6714a18d02d6ec485e57133acc14ad68c300
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-07-27 21:50:27 +0200

    Create AboutPage.js

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components';
+import ImageSection from '../Components/About/ImageSection';
+import Title from '../Components/About/Title';
+import {MainLayout} from '../styles/Layouts';
+import SkillsSection from '../Components/About/SkillsSection';
+
+function AboutPage() {
+    return (
+        <MainLayout>
+            <AboutStyled >
+                <Title title={'About Me'} span={'About Me'} />
+                <ImageSection />
+                <SkillsSection />                
+            </AboutStyled >
+        </MainLayout>
+            
+    )
+}
+
+const AboutStyled = styled.section`
+    
+`;
+
+export default AboutPage
