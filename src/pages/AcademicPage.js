aa9ae142e25dfd324fd1770de690c8a2982d6c0a
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-08-03 20:15:43 +0200

    Create AcademicPage.js

---
+++ src/pages/AcademicPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MainLayout} from '../styles/Layouts';
+import Academic from '../Components/academic/Academic';
+
+function AcademicPage() {
+    return (
+        <MainLayout>            
+            <Academic />
+        </MainLayout>
+    )
+}
+
+export default AcademicPage;
