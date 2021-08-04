9fdd411c537a92da0e17617208aaaf333eed5ec5
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-08-04 20:05:59 +0200

    Create ExperiencePage.js

---
+++ src/pages/ExperiencePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MainLayout} from '../styles/Layouts';
+import Experience from '../Components/experience/Experience';
+
+function ExperiencePage() {
+    return (
+        <MainLayout>            
+            <Experience />
+        </MainLayout>
+    )
+}
+
+export default ExperiencePage
