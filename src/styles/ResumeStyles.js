81e05aeadfb4f854756fad4f9125dcdeb822d3d5
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-08-04 20:06:08 +0200

    Create ResumeStyles.js

---
+++ src/styles/ResumeStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ResumeStyles = styled.section`
+    .small-title{
+        padding-bottom: 3rem;
+    }
+    /* .u-small-title-margin{
+        margin-top: 1rem;
+    } */
+
+    .Content{
+        border-left: 2px solid var(--border-color);
+    }
+`;
